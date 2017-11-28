e3358775ec053d62823ef19299d98958c8ff0b25
dominisz <dominisz@gmail.com>
2017-11-28 20:57:36 +0100

    Add test for list filling, reading elements, removing elements

--- README.md
+++ README.md
@@ -3,8 +3,18 @@
 
 ## W jaki sposób wygląda hierarchia interfejsów związana z kolekcjami?
 
+* interfejs bazowy Collection
+* interfejsy pochodne Collection: List, Set, Queue, Deque
+* interfejsem pochodnym po Set jest SortedSet
+* interfejs Map jest obok powyższej hierarchii
+* interfejsem pochodnym po Map jest SortedMap
 
 ## Jakie są podstawowe implementacje poszczególnych interfejsów?
+
+* implementacjami List są ArrayList, LinkedList
+* implementacjami Set są
+* implementacjami Queue są
+* implementacjami Deque są
 
 
 ## W których kolekcjach elementy są sortowane i w jaki sposób?
@@ -12,5 +22,9 @@
 
 ## Implementacje interfejsu List – złożoność operacji
 
+* ArrayList wykorzystuje wewnętrznie implementację tablicową
+* LinkedList wykorzystuje wewnętrznie implementację wskaźnikową
+* różnice pojawiają się podczas: dodawanie na początek listy, pobieranie i-tego elementu listy, usuwanie pierwszego elementu listy
 
-##Napisać aplikację wykorzystującą różne kolekcje i obrazującą różnice
+
+## Napisać aplikację wykorzystującą różne kolekcje i obrazującą różnice
--- src/main/java/pl/sdacademy/App.java
+++ src/main/java/pl/sdacademy/App.java
@@ -1,13 +1,72 @@
 package pl.sdacademy;
 
+import com.sun.xml.internal.ws.policy.spi.PolicyAssertionValidator;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final int LIST_SIZE = 100_000;
+
+    public static void main( String[] args ) {
+        List<Integer> arrayList = createArrayList();
+        List<Integer> linkedList = createLinkedList();
+
+        readAllElements(arrayList, "Czas czytania array list: ");
+        readAllElements(linkedList, "Czas czytania linked list: ");
+
+        deleteElements(arrayList, "Czas usuwania z array list: ");
+        deleteElements(linkedList, "Czas usuwania z linked list: ");
     }
+
+    private static void readAllElements(List<Integer> list, String message) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < LIST_SIZE; i++) {
+            Integer number = list.get(i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(message + (endTime - startTime));
+    }
+
+    private static void deleteElements(List<Integer> list, String message) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < LIST_SIZE; i++) {
+            Integer number = list.remove(0);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(message + (endTime - startTime));
+    }
+
+    //metoda tworzy LinkedList i wypełnia ją kolejnymi wartościami
+    //mierzy czas wypełnienia
+    private static List<Integer> createLinkedList() {
+        LinkedList<Integer> result = new LinkedList<>();
+        fillList(result, "Czas wypelnienia linked list: ");
+        return result;
+    }
+
+    //metoda tworzy LinkedList i wypełnia ją kolejnymi wartościami
+    //mierzy czas wypełnienia
+    private static List<Integer> createArrayList() {
+        ArrayList<Integer> result = new ArrayList<>();
+        fillList(result, "Czas wypelnienia array list: ");
+        return result;
+    }
+
+    private static void fillList(List<Integer> list, String message) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < LIST_SIZE; i++) {
+            list.add(0, i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(message + (endTime - startTime));
+    }
+
 }
